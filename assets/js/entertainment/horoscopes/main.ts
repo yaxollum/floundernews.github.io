34b63cda7f864e27d3619351db679ed10132437c
Peter Ye <yaxollum@gmail.com>
2021-12-01 12:11:11 -0500

    implement buttons (still need dates and symbols)

--- assets/js/entertainment/horoscopes/main.ts
+++ assets/js/entertainment/horoscopes/main.ts
@@ -1,85 +1,3 @@
-function riskAccepted() {
-  document.getElementById("horoscopes")!.style.display = "block";
-  document.getElementById("warning-button")!.style.display = "none";
-  const DATA = [
-    new Horoscope(
-      "Aries",
-      new CalendarDate(Month.March, 21),
-      new CalendarDate(Month.April, 19),
-      "Hot-headed as your sign, the Ram. Be kind this week and avoid butting heads. Remember to get sheared regularly to avoid overheating."
-    ),
-    new Horoscope(
-      "Taurus",
-      new CalendarDate(Month.April, 20),
-      new CalendarDate(Month.May, 20),
-      "Ambitious and determined, this year try to chill out a bit. High blood pressure will only harm you long term. Your friends all think you’re a try-hard. Only wear plaid this week, or else."
-    ),
-    new Horoscope(
-      "Gemini",
-      new CalendarDate(Month.May, 21),
-      new CalendarDate(Month.June, 20),
-      "One of the more misunderstood of the signs, remember: you’re not two-faced - you’re multi-faced. Own it! Also, go vegan this winter to avoid a tragic injury. Please."
-    ),
-    new Horoscope(
-      "Cancer",
-      new CalendarDate(Month.June, 21),
-      new CalendarDate(Month.July, 22),
-      "Usually an emotional wreck, this season, you will be surprisingly under control. What will you be under control of? I highly doubt it will be yourself. It’s not like I can see the future or anything."
-    ),
-    new Horoscope(
-      "Leo",
-      new CalendarDate(Month.July, 23),
-      new CalendarDate(Month.August, 22),
-      "Always thirsty for the spotlight, this month, decide whether you should clean your room. Today, confess to a crime you committed. If you didn’t commit it, even better."
-    ),
-    new Horoscope(
-      "Virgo",
-      new CalendarDate(Month.August, 23),
-      new CalendarDate(Month.September, 22),
-      "You have overcome so many adversaries this year, except one: yourself. Quit making excuses and be a better you. Avoid tall cliffs and rabid wolves."
-    ),
-    new Horoscope(
-      "Libra",
-      new CalendarDate(Month.September, 23),
-      new CalendarDate(Month.October, 22),
-      "Due to the position of the sun in the sky (probably somewhere in the middle), today you will have some amount of successes and failures."
-    ),
-    new Horoscope(
-      "Scorpio",
-      new CalendarDate(Month.October, 23),
-      new CalendarDate(Month.November, 21),
-      "Haha, look at your sign! It’s literally just an “M”. Look, I’m not saying that there’s anything wrong with the letter “M”, it’s just that I think you need a more interesting sign. At least you put in more effort than Aquarius over there."
-    ),
-    new Horoscope(
-      "Sagittarius",
-      new CalendarDate(Month.November, 22),
-      new CalendarDate(Month.December, 19),
-      "This month, your sign will be in full power. You will feel confident, self-assured, and strong; you will feel ready to tackle anything head on. Key word, “feel”."
-    ),
-    new Horoscope(
-      "Capricorn",
-      new CalendarDate(Month.December, 20),
-      new CalendarDate(Month.January, 19),
-      "This sign won’t tolerate bad vibes. Luckily, vibes are a figment of your imagination. Just vibe them away. Is this horoscope giving you bad vibes? What a shame."
-    ),
-    new Horoscope(
-      "Aquarius",
-      new CalendarDate(Month.January, 20),
-      new CalendarDate(Month.February, 18),
-      "You’ve worked yourself crazy all year, so sleep in and unwind this winter break. You will find this difficult now that the break is over. Scorpio’s talking trash about you."
-    ),
-    new Horoscope(
-      "Pisces",
-      new CalendarDate(Month.February, 19),
-      new CalendarDate(Month.March, 20),
-      "The moon is waning and Mackenzus is in its second semester. Change is good, but only sometimes. Go to school, spend time with friends, and read The Flounder."
-    ),
-  ];
-  document.getElementById("horoscope-name")!.innerHTML = DATA[0].name;
-  document.getElementById("horoscope-description")!.innerHTML =
-    DATA[0].description;
-}
-
 enum Month {
   January,
   February,
@@ -96,7 +14,7 @@ enum Month {
 }
 
 class CalendarDate {
-  constructor(public month: Month, public day: number) {}
+  constructor(public month: Month, public day: number) { }
 }
 
 class Horoscope {
@@ -105,5 +23,105 @@ class Horoscope {
     public firstDate: CalendarDate,
     public lastDate: CalendarDate,
     public description: string
-  ) {}
+  ) { }
 }
+
+const DATA = [
+  new Horoscope(
+    "Aries",
+    new CalendarDate(Month.March, 21),
+    new CalendarDate(Month.April, 19),
+    "Hot-headed as your sign, the Ram. Be kind this week and avoid butting heads. Remember to get sheared regularly to avoid overheating."
+  ),
+  new Horoscope(
+    "Taurus",
+    new CalendarDate(Month.April, 20),
+    new CalendarDate(Month.May, 20),
+    "Ambitious and determined, this year try to chill out a bit. High blood pressure will only harm you long term. Your friends all think you’re a try-hard. Only wear plaid this week, or else."
+  ),
+  new Horoscope(
+    "Gemini",
+    new CalendarDate(Month.May, 21),
+    new CalendarDate(Month.June, 20),
+    "One of the more misunderstood of the signs, remember: you’re not two-faced - you’re multi-faced. Own it! Also, go vegan this winter to avoid a tragic injury. Please."
+  ),
+  new Horoscope(
+    "Cancer",
+    new CalendarDate(Month.June, 21),
+    new CalendarDate(Month.July, 22),
+    "Usually an emotional wreck, this season, you will be surprisingly under control. What will you be under control of? I highly doubt it will be yourself. It’s not like I can see the future or anything."
+  ),
+  new Horoscope(
+    "Leo",
+    new CalendarDate(Month.July, 23),
+    new CalendarDate(Month.August, 22),
+    "Always thirsty for the spotlight, this month, decide whether you should clean your room. Today, confess to a crime you committed. If you didn’t commit it, even better."
+  ),
+  new Horoscope(
+    "Virgo",
+    new CalendarDate(Month.August, 23),
+    new CalendarDate(Month.September, 22),
+    "You have overcome so many adversaries this year, except one: yourself. Quit making excuses and be a better you. Avoid tall cliffs and rabid wolves."
+  ),
+  new Horoscope(
+    "Libra",
+    new CalendarDate(Month.September, 23),
+    new CalendarDate(Month.October, 22),
+    "Due to the position of the sun in the sky (probably somewhere in the middle), today you will have some amount of successes and failures."
+  ),
+  new Horoscope(
+    "Scorpio",
+    new CalendarDate(Month.October, 23),
+    new CalendarDate(Month.November, 21),
+    "Haha, look at your sign! It’s literally just an “M”. Look, I’m not saying that there’s anything wrong with the letter “M”, it’s just that I think you need a more interesting sign. At least you put in more effort than Aquarius over there."
+  ),
+  new Horoscope(
+    "Sagittarius",
+    new CalendarDate(Month.November, 22),
+    new CalendarDate(Month.December, 19),
+    "This month, your sign will be in full power. You will feel confident, self-assured, and strong; you will feel ready to tackle anything head on. Key word, “feel”."
+  ),
+  new Horoscope(
+    "Capricorn",
+    new CalendarDate(Month.December, 20),
+    new CalendarDate(Month.January, 19),
+    "This sign won’t tolerate bad vibes. Luckily, vibes are a figment of your imagination. Just vibe them away. Is this horoscope giving you bad vibes? What a shame."
+  ),
+  new Horoscope(
+    "Aquarius",
+    new CalendarDate(Month.January, 20),
+    new CalendarDate(Month.February, 18),
+    "You’ve worked yourself crazy all year, so sleep in and unwind this winter break. You will find this difficult now that the break is over. Scorpio’s talking trash about you."
+  ),
+  new Horoscope(
+    "Pisces",
+    new CalendarDate(Month.February, 19),
+    new CalendarDate(Month.March, 20),
+    "The moon is waning and Mackenzus is in its second semester. Change is good, but only sometimes. Go to school, spend time with friends, and read The Flounder."
+  ),
+];
+
+function showHoroscopeTab(index: number) {
+  document.getElementById("horoscope-name")!.innerHTML = DATA[index].name;
+  document.getElementById("horoscope-description")!.innerHTML =
+    DATA[index].description;
+}
+
+function addHoroscopeButtonToDocument(h: Horoscope, index: number) {
+  let button = document.createElement("button")
+  button.innerText = h.name
+  button.onclick = () => showHoroscopeTab(index)
+  document.getElementById("horoscope-tabs")!.appendChild(button)
+}
+
+function riskAccepted() {
+  document.getElementById("horoscopes")!.style.display = "";
+  document.getElementById("warning-button")!.style.display = "none";
+
+  DATA.forEach(addHoroscopeButtonToDocument);
+
+
+
+}
+
+
