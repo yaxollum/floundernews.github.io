beafeffe7f11a33d3737da0ca13216312f052876
Peter Ye <yaxollum@gmail.com>
2022-02-01 17:25:42 -0500

    add generate maze and show solution

--- tmp_maze/index.js
+++ tmp_maze/index.js
@@ -1,10 +1,11 @@
 "use strict";
 console.log("hello!");
-const cols = 8;
-const rows = 8;
-let currentPositionId = 0;
-let adj = [];
+const DEFAULT_ROWS = 15;
+const DEFAULT_COLS = 15;
+let cols;
+let rows;
 let boxSize;
+let c;
 let ctx;
 function coordToId(x, y) {
     return x * rows + y;
@@ -12,8 +13,9 @@ function coordToId(x, y) {
 function idToCoord(id) {
     return [Math.floor(id / rows), id % rows];
 }
-function randomBoolean() {
-    return Math.random() < 0.5;
+function idToGridCoord(id) {
+    let [x, y] = idToCoord(id);
+    return [(x + 1.5) * boxSize, (y + 1.5) * boxSize];
 }
 function drawGridLine(x1, y1, x2, y2) {
     ctx.beginPath();
@@ -21,21 +23,25 @@ function drawGridLine(x1, y1, x2, y2) {
     ctx.lineTo((x2 + 1) * boxSize, (y2 + 1) * boxSize);
     ctx.stroke();
 }
-function paintCanvas() {
-    let dpi = window.devicePixelRatio;
-    let c = document.getElementById("maze-canvas");
-    c.width *= dpi;
-    c.height *= dpi;
-    ctx = c.getContext("2d");
-    console.log(`DPI=${dpi}`);
-    ctx.scale(dpi, dpi);
+function getInputNumOrDefault(id, defaultVal) {
+    let s = document.getElementById(id).value;
+    let i = parseInt(s);
+    if (i != NaN && i >= 1) {
+        return i;
+    }
+    else {
+        return defaultVal;
+    }
+}
+function generateMaze() {
+    let currentPositionId = 0;
+    let adj = [];
+    let keyboardDisabled = false;
+    rows = getInputNumOrDefault("maze-rows", 15);
+    cols = getInputNumOrDefault("maze-cols", 15);
+    console.log(`${rows} ROWS ${cols} COLS`);
     boxSize = c.width / (cols + 2);
-    console.log(`boxSize=${boxSize}`);
     c.height = boxSize * (rows + 2);
-    ctx.beginPath();
-    ctx.rect(boxSize, boxSize, boxSize * cols, boxSize * rows);
-    ctx.stroke();
-    drawCharacter(false);
     let unionFind = [];
     let prev = [];
     for (let i = 0; i < cols * rows; ++i) {
@@ -106,6 +112,7 @@ function paintCanvas() {
         path.push(i);
         i = prev[i];
     }
+    path.push(startCell);
     let indexCut = Math.floor(path.length / 2);
     let uCutWall = path[indexCut];
     let vCutWall = path[indexCut + 1];
@@ -113,56 +120,94 @@ function paintCanvas() {
     adj[vCutWall].splice(adj[vCutWall].indexOf(uCutWall), 1);
     let [ux, uy] = idToCoord(uCutWall);
     let [vx, vy] = idToCoord(vCutWall);
-    ctx.strokeStyle = "red";
-    if (ux == vx) {
-        drawGridLine(ux, (uy + vy + 1) / 2, ux + 1, (uy + vy + 1) / 2);
-    }
-    else if (uy == vy) {
-        drawGridLine((ux + vx + 1) / 2, uy, (ux + vx + 1) / 2, uy + 1);
-    }
-    ctx.strokeStyle = "black";
-    for (let wall of mazeWalls) {
-        let [ux, uy] = idToCoord(wall.u);
-        let [vx, vy] = idToCoord(wall.v);
-        if (wall.type == "horizontal") {
-            drawGridLine(ux, (uy + vy + 1) / 2, ux + 1, (uy + vy + 1) / 2);
+    function drawMaze(drawImpossibleWall) {
+        ctx.clearRect(0, 0, c.width, c.height);
+        ctx.beginPath();
+        ctx.rect(boxSize, boxSize, boxSize * cols, boxSize * rows);
+        ctx.stroke();
+        if (drawImpossibleWall) {
+            let wallLine = [0, 0, 0, 0];
+            if (ux == vx) {
+                wallLine = [ux, (uy + vy + 1) / 2, ux + 1, (uy + vy + 1) / 2];
+            }
+            else if (uy == vy) {
+                wallLine = [(ux + vx + 1) / 2, uy, (ux + vx + 1) / 2, uy + 1];
+            }
+            drawGridLine(...wallLine);
         }
-        else {
-            drawGridLine((ux + vx + 1) / 2, uy, (ux + vx + 1) / 2, uy + 1);
+        for (let wall of mazeWalls) {
+            let [ux, uy] = idToCoord(wall.u);
+            let [vx, vy] = idToCoord(wall.v);
+            if (wall.type == "horizontal") {
+                drawGridLine(ux, (uy + vy + 1) / 2, ux + 1, (uy + vy + 1) / 2);
+            }
+            else {
+                drawGridLine((ux + vx + 1) / 2, uy, (ux + vx + 1) / 2, uy + 1);
+            }
         }
+        drawCharacter(currentPositionId, false);
     }
+    drawMaze(true);
+    window.onkeydown = (e) => {
+        if (keyboardDisabled) {
+            return;
+        }
+        let delta_x = 0;
+        let delta_y = 0;
+        if (e.key.startsWith("Arrow")) {
+            e.preventDefault();
+        }
+        if (e.key == "ArrowLeft" || e.key == "a") {
+            delta_x = -1;
+        }
+        else if (e.key == "ArrowRight" || e.key == "d") {
+            delta_x = 1;
+        }
+        else if (e.key == "ArrowUp" || e.key == "w") {
+            delta_y = -1;
+        }
+        else if (e.key == "ArrowDown" || e.key == "s") {
+            delta_y = 1;
+        }
+        let [current_x, current_y] = idToCoord(currentPositionId);
+        let [new_x, new_y] = [current_x + delta_x, current_y + delta_y];
+        let newPositionId = coordToId(new_x, new_y);
+        if (adj[currentPositionId].includes(newPositionId)) {
+            drawCharacter(currentPositionId, true);
+            drawCharacter(newPositionId, false);
+            currentPositionId = newPositionId;
+        }
+    };
+    document.getElementById("show-solution").onclick = () => {
+        keyboardDisabled = true;
+        drawMaze(false);
+        ctx.strokeStyle = "lightblue";
+        ctx.beginPath();
+        ctx.lineWidth = boxSize / 6;
+        ctx.moveTo(...idToGridCoord(path[0]));
+        for (let i = 1; i < path.length; ++i) {
+            ctx.lineTo(...idToGridCoord(path[i]));
+        }
+        ctx.stroke();
+    };
 }
-function drawCharacter(erase) {
-    let [current_x, current_y] = idToCoord(currentPositionId);
+function drawCharacter(positionId, erase) {
+    let [current_x, current_y] = idToGridCoord(positionId);
     ctx.beginPath();
-    ctx.arc((current_x + 1.5) * boxSize, (current_y + 1.5) * boxSize, erase ? boxSize / 2.5 : boxSize / 3, 0, 2 * Math.PI);
+    ctx.arc(current_x, current_y, erase ? boxSize / 2.5 : boxSize / 3, 0, 2 * Math.PI);
     ctx.fillStyle = erase ? "white" : "green";
     ctx.fill();
 }
-window.addEventListener("keydown", (e) => {
-    e.preventDefault();
-    let delta_x = 0;
-    let delta_y = 0;
-    if (e.key == "ArrowLeft" || e.key == "a") {
-        delta_x = -1;
-    }
-    else if (e.key == "ArrowRight" || e.key == "d") {
-        delta_x = 1;
-    }
-    else if (e.key == "ArrowUp" || e.key == "w") {
-        delta_y = -1;
-    }
-    else if (e.key == "ArrowDown" || e.key == "s") {
-        delta_y = 1;
-    }
-    console.log(delta_x, delta_y);
-    let [current_x, current_y] = idToCoord(currentPositionId);
-    let [new_x, new_y] = [current_x + delta_x, current_y + delta_y];
-    let newPositionId = coordToId(new_x, new_y);
-    if (adj[currentPositionId].includes(newPositionId)) {
-        drawCharacter(true);
-        currentPositionId = newPositionId;
-        drawCharacter(false);
-    }
-});
-window.onload = paintCanvas;
+window.onload = () => {
+    let dpi = window.devicePixelRatio;
+    c = document.getElementById("maze-canvas");
+    c.width *= dpi;
+    c.height *= dpi;
+    ctx = c.getContext("2d");
+    ctx.scale(dpi, dpi);
+    document.getElementById("maze-rows").value =
+        DEFAULT_ROWS.toString();
+    document.getElementById("maze-cols").value =
+        DEFAULT_COLS.toString();
+};
+document.getElementById("generate-maze").onclick = generateMaze;
--- tmp_maze/index.ts
+++ tmp_maze/index.ts
@@ -1,12 +1,12 @@
 console.log("hello!");
 
-const cols = 8;
-const rows = 8;
-
-let currentPositionId = 0;
-let adj: number[][] = [];
+const DEFAULT_ROWS = 15;
+const DEFAULT_COLS = 15;
+let cols: number;
+let rows: number;
 
 let boxSize: number;
+let c: HTMLCanvasElement;
 let ctx: CanvasRenderingContext2D;
 
 interface Wall {
@@ -23,8 +23,9 @@ function idToCoord(id: number): [number, number] {
   return [Math.floor(id / rows), id % rows];
 }
 
-function randomBoolean(): boolean {
-  return Math.random() < 0.5;
+function idToGridCoord(id: number): [number, number] {
+  let [x, y] = idToCoord(id);
+  return [(x + 1.5) * boxSize, (y + 1.5) * boxSize];
 }
 
 function drawGridLine(x1: number, y1: number, x2: number, y2: number) {
@@ -33,26 +34,26 @@ function drawGridLine(x1: number, y1: number, x2: number, y2: number) {
   ctx.lineTo((x2 + 1) * boxSize, (y2 + 1) * boxSize);
   ctx.stroke();
 }
-function paintCanvas() {
-  let dpi = window.devicePixelRatio;
-  let c = document.getElementById("maze-canvas") as HTMLCanvasElement;
-  c.width *= dpi;
-  c.height *= dpi;
-  ctx = c.getContext("2d")!;
 
-  console.log(`DPI=${dpi}`);
-  ctx.scale(dpi, dpi);
+function getInputNumOrDefault(id: string, defaultVal: number) {
+  let s = (document.getElementById(id) as HTMLInputElement).value;
+  let i = parseInt(s);
+  if (i != NaN && i >= 1) {
+    return i;
+  } else {
+    return defaultVal;
+  }
+}
+function generateMaze() {
+  let currentPositionId = 0;
+  let adj: number[][] = [];
+  let keyboardDisabled = false;
 
+  rows = getInputNumOrDefault("maze-rows", 15);
+  cols = getInputNumOrDefault("maze-cols", 15);
+  console.log(`${rows} ROWS ${cols} COLS`);
   boxSize = c.width / (cols + 2);
-  console.log(`boxSize=${boxSize}`);
-
   c.height = boxSize * (rows + 2);
-  ctx.beginPath();
-
-  ctx.rect(boxSize, boxSize, boxSize * cols, boxSize * rows);
-  ctx.stroke();
-
-  drawCharacter(false);
 
   let unionFind: number[] = [];
   let prev: number[] = [];
@@ -121,12 +122,13 @@ function paintCanvas() {
     }
   };
   dfs(startCell, startCell);
-  let path = [];
+  let path: number[] = [];
   let i = endCell;
   while (i != startCell) {
     path.push(i);
     i = prev[i];
   }
+  path.push(startCell);
 
   let indexCut = Math.floor(path.length / 2);
   let uCutWall = path[indexCut];
@@ -135,31 +137,84 @@ function paintCanvas() {
   adj[vCutWall].splice(adj[vCutWall].indexOf(uCutWall), 1);
   let [ux, uy] = idToCoord(uCutWall);
   let [vx, vy] = idToCoord(vCutWall);
-  ctx.strokeStyle = "red";
-  if (ux == vx) {
-    drawGridLine(ux, (uy + vy + 1) / 2, ux + 1, (uy + vy + 1) / 2);
-  } else if (uy == vy) {
-    drawGridLine((ux + vx + 1) / 2, uy, (ux + vx + 1) / 2, uy + 1);
-  }
-  ctx.strokeStyle = "black";
 
-  for (let wall of mazeWalls) {
-    let [ux, uy] = idToCoord(wall.u);
-    let [vx, vy] = idToCoord(wall.v);
-    if (wall.type == "horizontal") {
-      drawGridLine(ux, (uy + vy + 1) / 2, ux + 1, (uy + vy + 1) / 2);
-    } else {
-      drawGridLine((ux + vx + 1) / 2, uy, (ux + vx + 1) / 2, uy + 1);
+  function drawMaze(drawImpossibleWall: boolean) {
+    ctx.clearRect(0, 0, c.width, c.height);
+    ctx.beginPath();
+
+    ctx.rect(boxSize, boxSize, boxSize * cols, boxSize * rows);
+    ctx.stroke();
+    if (drawImpossibleWall) {
+      let wallLine: [number, number, number, number] = [0, 0, 0, 0];
+      if (ux == vx) {
+        wallLine = [ux, (uy + vy + 1) / 2, ux + 1, (uy + vy + 1) / 2];
+      } else if (uy == vy) {
+        wallLine = [(ux + vx + 1) / 2, uy, (ux + vx + 1) / 2, uy + 1];
+      }
+      drawGridLine(...wallLine);
     }
+
+    for (let wall of mazeWalls) {
+      let [ux, uy] = idToCoord(wall.u);
+      let [vx, vy] = idToCoord(wall.v);
+      if (wall.type == "horizontal") {
+        drawGridLine(ux, (uy + vy + 1) / 2, ux + 1, (uy + vy + 1) / 2);
+      } else {
+        drawGridLine((ux + vx + 1) / 2, uy, (ux + vx + 1) / 2, uy + 1);
+      }
+    }
+    drawCharacter(currentPositionId, false);
   }
+  drawMaze(true);
+
+  window.onkeydown = (e) => {
+    if (keyboardDisabled) {
+      return;
+    }
+    let delta_x = 0;
+    let delta_y = 0;
+    if (e.key.startsWith("Arrow")) {
+      e.preventDefault();
+    }
+    if (e.key == "ArrowLeft" || e.key == "a") {
+      delta_x = -1;
+    } else if (e.key == "ArrowRight" || e.key == "d") {
+      delta_x = 1;
+    } else if (e.key == "ArrowUp" || e.key == "w") {
+      delta_y = -1;
+    } else if (e.key == "ArrowDown" || e.key == "s") {
+      delta_y = 1;
+    }
+    let [current_x, current_y] = idToCoord(currentPositionId);
+    let [new_x, new_y] = [current_x + delta_x, current_y + delta_y];
+    let newPositionId = coordToId(new_x, new_y);
+    if (adj[currentPositionId].includes(newPositionId)) {
+      drawCharacter(currentPositionId, true);
+      drawCharacter(newPositionId, false);
+      currentPositionId = newPositionId;
+    }
+  };
+
+  document.getElementById("show-solution")!.onclick = () => {
+    keyboardDisabled = true;
+    drawMaze(false);
+    ctx.strokeStyle = "lightblue";
+    ctx.beginPath();
+    ctx.lineWidth = boxSize / 6;
+    ctx.moveTo(...idToGridCoord(path[0]));
+    for (let i = 1; i < path.length; ++i) {
+      ctx.lineTo(...idToGridCoord(path[i]));
+    }
+    ctx.stroke();
+  };
 }
 
-function drawCharacter(erase: boolean) {
-  let [current_x, current_y] = idToCoord(currentPositionId);
+function drawCharacter(positionId: number, erase: boolean) {
+  let [current_x, current_y] = idToGridCoord(positionId);
   ctx.beginPath();
   ctx.arc(
-    (current_x + 1.5) * boxSize,
-    (current_y + 1.5) * boxSize,
+    current_x,
+    current_y,
     erase ? boxSize / 2.5 : boxSize / 3,
     0,
     2 * Math.PI
@@ -167,28 +222,18 @@ function drawCharacter(erase: boolean) {
   ctx.fillStyle = erase ? "white" : "green";
   ctx.fill();
 }
-window.addEventListener("keydown", (e) => {
-  e.preventDefault();
-  let delta_x = 0;
-  let delta_y = 0;
-  if (e.key == "ArrowLeft" || e.key == "a") {
-    delta_x = -1;
-  } else if (e.key == "ArrowRight" || e.key == "d") {
-    delta_x = 1;
-  } else if (e.key == "ArrowUp" || e.key == "w") {
-    delta_y = -1;
-  } else if (e.key == "ArrowDown" || e.key == "s") {
-    delta_y = 1;
-  }
-  console.log(delta_x, delta_y);
-  let [current_x, current_y] = idToCoord(currentPositionId);
-  let [new_x, new_y] = [current_x + delta_x, current_y + delta_y];
-  let newPositionId = coordToId(new_x, new_y);
-  if (adj[currentPositionId].includes(newPositionId)) {
-    drawCharacter(true);
-    currentPositionId = newPositionId;
-    drawCharacter(false);
-  }
-});
 
-window.onload = paintCanvas;
+window.onload = () => {
+  let dpi = window.devicePixelRatio;
+  c = document.getElementById("maze-canvas") as HTMLCanvasElement;
+  c.width *= dpi;
+  c.height *= dpi;
+  ctx = c.getContext("2d")!;
+  ctx.scale(dpi, dpi);
+  (document.getElementById("maze-rows") as HTMLInputElement).value =
+    DEFAULT_ROWS.toString();
+  (document.getElementById("maze-cols") as HTMLInputElement).value =
+    DEFAULT_COLS.toString();
+};
+
+document.getElementById("generate-maze")!.onclick = generateMaze;
